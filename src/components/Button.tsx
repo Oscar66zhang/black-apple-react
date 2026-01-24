64e6c92db989e51e204cbf376ff9aa765c8055ab
Oscar Zhang <1711381142@qq.com>
2026-01-24 11:31:27 +0800

    "Update Button"

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,8 +5,15 @@ interface ButtonProps {
   onClick?: () => void;
 }
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({
+  title,
+  variant = "primary",
+  disabled,
+  onClick,
+}: ButtonProps) => {
+  let className = `
+  px-5 py-2 rounded-md border
+    transition-colors duration-200`;
 };
 
 export default Button;
